5ce2136ec7a577cc6428aceab3584067f1ac638e
Sahil Makhijani <svmakhijani94@gmail.com>
2020-09-06 01:22:01 -0700

    Added FlexLayout to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { Categories } from './constants/strings';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
-  declarations: [
-    Categories
-  ],
+  declarations: [],
   exports: [
-    Categories
+    FlexLayoutModule
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule
   ]
 })
 export class SharedModule { }
