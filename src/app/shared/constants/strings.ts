70ea92ad0d0fca5132a2b0362886f415785bf08c
Sahil Makhijani <svmakhijani94@gmail.com>
2021-02-09 16:13:23 -0700

    Added string constants for about us section

--- src/app/shared/constants/strings.ts
+++ src/app/shared/constants/strings.ts
@@ -36,3 +36,16 @@ export const FooterLinks: string[] = [
   'team',
   'contact'
 ];
+
+export const AboutUs = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suscipit, urna a bibendum fermentum, magna turpis porttitor magna, sit amet ullamcorper ipsum elit a purus. Phasellus tincidunt ligula in leo laoreet consequat ac id purus. Pellentesque vel dolor ullamcorper, efficitur massa nec, finibus sapien. Quisque egestas feugiat sapien in tristique. Sed in porta odio. In hac habitasse platea dictumst. Donec nec placerat nisi.';
+
+export const Management = {
+  managing_director: '[Managing Director name]',
+  sales_manager: '[Sales Manager name]',
+  pr_manager: '[Public Relations Manager name]',
+  prod_manager: '[Production Manager name]',
+  hr_manager: '[Senior HR Manager name]',
+  finance_manager: '[Finance Manager name]'
+};
+
+export const ContactUs = 'Never speak to us. We don\'t care what you have to say. We\'re not real.';
