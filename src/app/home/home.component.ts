48e664c88576a4a060f3f5780d9393bd98115cc6
Sahil Makhijani <svmakhijani94@gmail.com>
2021-02-17 14:23:45 -0700

    Added category tiles to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { DarkModeService } from '../core/services/dark-mode.service';
+import { Categories } from '../shared/constants/strings';
+import { Theme } from '../shared/constants/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  categories: string[];
+  theme: Theme = 'dark';
+  unsubscribe = new Subject();
+
+  constructor(private darkModeService: DarkModeService) {
+    this.categories = Categories;
+  }
 
   ngOnInit(): void {
+    this.darkModeService.isDarkOn.pipe(takeUntil(this.unsubscribe)).subscribe((bool) => {
+      this.theme = (bool) ? 'light' : 'dark';
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
