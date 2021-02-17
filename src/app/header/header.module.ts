c147295ec193b4d8512f1d100208b6251e0731f7
Sahil Makhijani <svmakhijani94@gmail.com>
2021-02-17 16:14:32 -0700

    Added links to category pages from header menus

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
 import { SharedModule } from '../shared/shared.module';
@@ -10,6 +11,7 @@ import { SharedModule } from '../shared/shared.module';
   exports: [ HeaderComponent ],
   imports: [
     CommonModule,
+    RouterModule,
     SharedModule
   ]
 })
